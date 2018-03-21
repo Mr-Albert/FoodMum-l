05388d6f99c52c898e44dcbbf5ac8572a429f63d
Mina <mina.reda.k.albert@gmail.com>
2018-03-21 16:09:11 +0300

    Scafollded most of the ui
    don't forget to go in public/ and
    bower install

---
+++ FoodMUM/resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+@include('partials.head')
+<body class="skin-blue sidebar-mini sidebar-collapse">
+<div class="wrapper">
+
+    <!-- Header -->
+    @include('partials.topbar')
+
+    <!-- Sidebar -->
+    @include('partials.sidebar')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                {{ $page_title or "Page Title" }}
+                <small>{{ $page_description or null }}</small>
+            </h1>
+            <!-- You can dynamically generate breadcrumbs here -->
+            {{--  <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+                <li class="active">Here</li>
+            </ol>  --}}
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <!-- Your Page Content Here -->
+            @yield('content')
+        </section><!-- /.content -->
+    </div><!-- /.content-wrapper -->
+
+    <!-- Footer -->
+    @include('partials.footer')
+
+</div><!-- ./wrapper -->
+
+</body>
+</html>
---
+++ FoodMUM/resources/views/partials/sidebar.blade.php
@@ -0,0 +1,36 @@
+<!-- Left side column. contains the sidebar -->
+<ul class="sidebar-menu" data-widget="tree">
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset("/bower_components/admin-lte/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image" />
+            </div>
+            <div class="pull-left info">
+                <p style='font-size: 15px;width: 100%;word-break: break-all;white-space: normal;'>Mina Reda Albert  ,make it dynamic</p>
+                <!-- any additional info -->
+                
+            </div>
+        </div>
+        <!-- /.search form -->
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu">
+            
+            <li class="header">Pages</li>
+            <li class="active"><a href="#"><i class="fa fa-link"></i><span>Link</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i><span>Another Link</span></a></li>
+            <li class="treeview">
+                <a href="#"><i class="fa fa-sitemap"></i><span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Link in level 2</a></li>
+                    <li><a href="#">Link in level 2</a></li>
+                </ul>
+            </li>
+        </ul><!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
