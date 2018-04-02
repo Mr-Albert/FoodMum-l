a702ef4e2093eee748099571e6b6180e04bed4f3
Mina <mina.reda.k.albert@gmail.com>
2018-04-02 18:42:04 +0300

    creating new whole project using the admin lte provider

--- FoodMUM/resources/views/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        Anything Mina want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright © 2018 <a href="#">Company</a>.</strong> All rights reserved.
-</footer>
